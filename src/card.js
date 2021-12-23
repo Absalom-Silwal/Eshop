ef33288989b7a5af4b721268ba6efdd5ea8ef079
Absalom <absalomsilwal93@gmail.com>
2021-12-23 08:47:43 +0545

    fetch data issue fixed

--- src/card.js
+++ src/card.js
@@ -4,57 +4,66 @@ import {  Link } from 'react-router-dom'
 import { cartAdd ,removeRedirect } from "./action/cart";
 
 export let Card = ()=>{
- 
-    const dispatch = useDispatch();
-    const fltr = useSelector(state=>state.filter.filter)
-    const choice= useSelector(state =>state.filter.choice )   
-    const cartObj =  useSelector(state =>state.cart.cartadd )
-   console.log(cartObj)
-
+   const dispatch = useDispatch();
+   const productList  = useSelector(state => state.fetchData.products)
+   const fltr = useSelector(state=>state.filter.filter)
+   const choice= useSelector(state =>state.filter.choice )
+   console.log(productList)
+  
+  
    
-   const productList = useSelector(state=>state.fetchData.products.data.product)
-   const renderFilterList = productList.filter((product)=>product.category[1]===choice).map((product,pos)=>{
-   const pic = `https://electronic-ecommerce.herokuapp.com/${product.image}`
-   console.log(cartObj[pos])
-        return(
-            <div className={classes.card} key={product.id-1}>
-             <img src={pic} alt="Denim Jeans" />
-            <h1>{product.name}</h1>
-            <p className={classes.price}>$19.99</p>
-           
-            <p>
-            <Link exact to="/cart"><p><button onClick={()=>(dispatch(cartAdd(product.id,1,0,cartObj[pos])))}>Add to Cart</button></p></Link>
-            </p>
-          
+   return(
+      <div>
+       {(productList.length===0)?(
+            <div>loading</div>
+         ):(
+            <div >
+               {(!fltr)?(
+                  <div className={classes.card_list}>
+                  {productList.data.product.map((product,pos)=>{
+                     const pic = `https://electronic-ecommerce.herokuapp.com/${product.image}`
+                        return(
+                        <div className={classes.card}>
+                        <img src={pic} alt="Denim Jeans" />
+                        <h1>{product.name}</h1>
+                        <p className={classes.price}>Rs {Number(product.price.slice(1,))*100}</p>
+                        <Link exact to="/cart"><p><button onClick={()=>(dispatch(cartAdd(product.id,1,0)))}>Add to Cart</button></p></Link>
+                        </div>
+                        
+                        )}
+                        )}
+                  </div>
+               ):(<div className={classes.card_list}>
+                  {productList.data.product.filter((product)=>product.category[1]===choice).map((product,pos)=>{
+                     const pic = `https://electronic-ecommerce.herokuapp.com/${product.image}`
+                        return(
+                           <div className={classes.card}>
+                           <img src={pic} alt="Denim Jeans" />
+                           <h1>{product.name}</h1>
+                           <p className={classes.price}>Rs{Number(product.price.slice(1,))*100}</p>
+                           <Link exact to="/cart"><p><button onClick={()=>(dispatch(cartAdd(product.id,1,0)))}>Add to Cart</button></p></Link>
+                           </div>
+                        )
+                        
+                        
+                     })}
+               </div>)}
+                
             </div>
-        )
-        
-     })   
-     const renderWholeList = productList.map((product)=>{
-      
-      const price = `Rs ${Number(product.price.slice(1,))*100}`
-      console.log(price)
-      const pic = `https://electronic-ecommerce.herokuapp.com/${product.image}`
-         return(
-            <div className={classes.card}>
-            <img src={pic} alt="Denim Jeans" />
-            <h1>{product.name}</h1>
-            <p className={classes.price}>{price}</p>
-             <Link exact to="/cart"><p><button onClick={()=>(dispatch(cartAdd(product.id,1,0)))}>Add to Cart</button></p></Link>
-            </div>
-         )
-     })
+               
             
-   if(!fltr){
-      
-        return <>{renderWholeList}</>
-       
-   }
-   else {
-    return <>{renderFilterList}</>
-   }
+         )}
+      </div>
+   )
+            
+         
+
+         
+     
+            
+  
 }
-    
+
       
         
    
